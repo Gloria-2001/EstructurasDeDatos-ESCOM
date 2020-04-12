ed360820933be226addde6ae1ca39250b0171278
Gloria Olivares <62816556+Gloria-2001@users.noreply.github.com>
2020-04-12 12:56:00 -0500

    Update cola.h

--- EstructuraCola/cola.h
+++ EstructuraCola/cola.h
@@ -37,58 +37,58 @@ typedef struct{
     int elemCola;               // Número de elementos insertados en la cola 
 }Cola;
 
-void iniciarQueue(Cola *queue){
-    queue->elemCola=0;
-    queue->pri=0;
-    queue->ult=0;
-    for(int i=0;i<maxCola;i++)
-        queue->queue[i]=-1;
+void iniciarQueue(Cola *queue){ // Función para incializar la cola. 
+    queue->elemCola=0;          // El número de elementos ingresados hasta ahora en la cola es 0. 
+    queue->pri=0;               // El índice del primer dato de la cola estará inicializado en 0. 
+    queue->ult=0;               // El índice del último dato de la cola estará inicializado en 0. 
+    for(int i=0;i<maxCola;i++)  // 
+        queue->queue[i]=-1;     //
 }
 
-int estaVacia(Cola *queue){
-    if(queue->elemCola==0)
-        return 1;
-    else
-        return 0;
+int estaVacia(Cola *queue){     // Función para verificar si la cola está vacía. 
+    if(queue->elemCola==0)      // Si el número de elementos ingresados es 0,
+        return 1;               // entonces se cumple.
+    else                        // De lo contrario,
+        return 0;               // no es verdadero, no se cumple. 
 }
 
-int estaLlena(Cola *queue){
-    if(queue->elemCola==maxCola)
-        return 1;
-    else
-        return 0;
+int estaLlena(Cola *queue){     // Función para verificar cola está llena. 
+    if(queue->elemCola==maxCola)// Si el número de elementos ingresados es igual a lo máximo que admite la cola,
+        return 1;               // entonces se cumple esta función. 
+    else                        // De lo contrario, 
+        return 0;               // no es verdadero, no se cumple. 
 }
 
-void enQueue(Cola *queue, int dato){
-    if(estaLlena(queue)){
-        printf("\nPila llena\n");
-    }else if(estaVacia(queue)){
-        queue->queue[0]=dato;
-        queue->elemCola++;
-    }else{
-        queue->queue[queue->elemCola]=dato;
-        queue->ult++;
-        queue->elemCola++;
+void enQueue(Cola *queue, int dato){  // Función para meter datos en la cola. 
+    if(estaLlena(queue)){             // Si se cumple la función estaLlena, 
+        printf("\nPila llena\n");     // se imprimirá que la pila está llena. 
+    }else if(estaVacia(queue)){       // De lo contrario y si se cumple la función estaVacia, 
+        queue->queue[0]=dato;         // el elemento cero del arreglo va a ser el dato. 
+        queue->elemCola++;            // Después, el número de elementos ingresados va a aumentar de uno por uno, 
+    }else{                            // De no cumplirse los if anteriores (si la cola no está vacía ni llena),  
+        queue->queue[queue->elemCola]=dato; // se va a agregar un elemento y ese será el nuevo dato; 
+        queue->ult++;                       // el índice del último elemento va a aumentar de uno por uno, conforme se metan los datos. 
+        queue->elemCola++;                  // También el número de elementos ingresados aumentará. 
     }
 }
 
-void deQueue(Cola *queue){
-    int aux,i;
-    if(estaVacia(queue)){
-        printf("\nPila vacia");
-    }else{
-        printf("Elemento eliminado: %2d",queue->queue[queue->pri]);
-        for(i=0;i<queue->elemCola-1;i++)
-            queue->queue[i]=queue->queue[i+1];
-        queue->elemCola--;
-        queue->ult--;
+void deQueue(Cola *queue){      // Función para sacar datos de la Cola. 
+    int aux,i;                  // Se declaran 2 variables enteras, una auxiliar. 
+    if(estaVacia(queue)){       // Si se cumple la función estaVacia, 
+        printf("\nPila vacia"); // entonces se va a imprimir que la Cola está vacía. 
+    }else{                      // De lo contrario,
+        printf("Elemento eliminado: %2d",queue->queue[queue->pri]); // Se va a imprimir, el elemento eliminado es x, y va a ser primer elemento metido.
+        for(i=0;i<queue->elemCola-1;i++)         // 
+            queue->queue[i]=queue->queue[i+1];   //
+        queue->elemCola--;      // Los elementos ingresados disminuirán uno por uno. 
+        queue->ult--;           // También el que esté catalogado como último elemento va a irse recorriendo. 
     }
 }
 
-void barrelQueue(Cola *queue){
-    if(estaVacia(queue))
-        printf("\nPila vacia");
-    else
-        for(int i=0;i<queue->elemCola;i++)
-            printf("%3d ",queue->queue[i]);
+void barrelQueue(Cola *queue){      // Función para recorrer datos en la Cola. 
+    if(estaVacia(queue))            // Si se cumple la función estaVacia, 
+        printf("\nPila vacia");     // entonces se imprime que Pila vacia. 
+    else                            // De lo contrario, 
+        for(int i=0;i<queue->elemCola;i++)      // 
+            printf("%3d ",queue->queue[i]);     //
 }
