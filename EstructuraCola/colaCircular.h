48027d2732b3a2327c31545aaeb199dae5d4998f
Gloria-2001 <olimen141@gmail.com>
2020-04-22 19:17:03 -0500

    Correcciones 2.0

--- EstructuraCola/colaCircular.h
+++ EstructuraCola/colaCircular.h
@@ -16,12 +16,12 @@ typedef struct{
 	int queue[MaxCola];
 	int pri;
 	int ult;
-	int elemCola;
+	int datosIngresados;
 }Cola;
 
 void iniciarCola(Cola *queue){
 	int i;
-    queue->elemCola=0;          
+    queue->datosIngresados=0;          
     queue->pri=0;               
     queue->ult=0;                
     for(i=0;i<MaxCola;i++)  
@@ -29,14 +29,14 @@ void iniciarCola(Cola *queue){
 }
 
 int ColaVacia(Cola *queue){
-	if(queue->elemCola==0)
+	if(queue->datosIngresados==0)
 		return 1;
 	else 
 		return 0; 
 }
 
 int ColaLlena(Cola *queue){
-	if(queue->elemCola==MaxCola)
+	if(queue->datosIngresados==MaxCola)
 		return 1;
 	else 
 		return 0;
@@ -45,34 +45,38 @@ int ColaLlena(Cola *queue){
 void meterCola(Cola *queue, int dato){   
     int i;
 	if(ColaLlena(queue)){              
-        printf("\nPila llena\n");      
+        printf("\nCola llena\n");      
     }else if(ColaVacia(queue)){        
         queue->queue[0]=dato; 
+		queue->datosIngresados++;
     }else{                              
-		queue->queue[((queue->ult)+1)%6]=dato;
-		queue->queue[((queue->ult)+1)%6]=queue->elemCola;
+		i=(queue->datosIngresados)%MaxCola;
+		queue->queue[i+(queue->pri)]=dato;
+		queue->datosIngresados++;
     }
 }
 
 void recorrer(Cola *queue){      
 	int i;
     if(ColaVacia(queue))             
-        printf("\nPila vacia");     
+        printf("\nCola vacia");     
     else                            
-		for(i=0;i<MaxCola;i++){
-			printf("%d",queue->queue[i]);
+		for(i=0;i<(queue->datosIngresados);i++){
+			printf("%d\t",queue[(i+(queue->pri))%(queue->datosIngresados)]);
 		}
 	}
  
-void sacarCola(Cola *queue){
-	int aux,i;                   
-    if(ColaVacia(queue)){        
-        printf("\nPila vacia");  
+int sacarCola(Cola *queue, int dato){                  
+    int i;
+	if(ColaVacia(queue)){        
+        printf("\nCola vacia");  
     }else{                      
-        printf("Elemento eliminado: %d",queue->pri); 
-        queue->queue[i]=queue->queue[i+1];
-		queue->queue[i]=((queue->pri)+1)%6;
-    }
+        printf("Elemento eliminado: %d",queue->queue[queue->pri]); 
+        queue->queue[queue->pri]=-1;
+		queue->pri=((queue->pri)+1)%MaxCola; 
+		queue->datosIngresados--;
+	}
+	return dato;
 }
 
 
