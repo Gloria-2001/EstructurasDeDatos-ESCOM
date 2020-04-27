0f8578901f5c63dbb7ed76ee5b5a31bb907b08af
Gloria-2001 <olimen141@gmail.com>
2020-04-27 09:20:21 -0500

    Definición de Operadores

--- Infija_Posfija/Infija_Posfija.c
+++ Infija_Posfija/Infija_Posfija.c
@@ -1,4 +1,12 @@
 #include "PilaDinamica.h"
+#define SUMA '+'
+#define RESTA '-'
+#define MULT '*'
+#define DIV '/'
+#define POT '^'
+#define PARENT_IN '('
+#define PARENT_OUT ')'
+
 
 int main(){
     struct nodo *pila00=(struct nodo*)malloc(sizeof(struct nodo));    // Guardará los operadores
