9b2e6c18fa847eca34917025b3ffef877ec0d692
Zait06 <zait-hdez@hotmail.com>
2020-04-27 08:51:26 -0500

    Infija Posfija

--- Infija_Posfija/Infija_Posfija.c
+++ Infija_Posfija/Infija_Posfija.c
@@ -1,7 +1,7 @@
 #include "PilaDinamica.h"
 
 int main(){
-    struct nodo *MiPtrCima=(struct nodo*)malloc(sizeof(struct nodo));
-    MiPtrCima->ptrSig=NULL;
+    struct nodo *pila00=(struct nodo*)malloc(sizeof(struct nodo));    // Guardará los operadores
+    pila00->ptrSig=NULL;
     char expresion[100];
 }
