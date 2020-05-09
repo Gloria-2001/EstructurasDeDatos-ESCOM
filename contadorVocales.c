5a83a2e9bc653af3f7b16c6a49a3446105b548cb
Gloria-2001 <olimen141@gmail.com>
2020-05-08 22:45:05 -0500

    Contador de vocales 2.0

--- contadorVocales.c
+++ contadorVocales.c
@@ -2,63 +2,30 @@
 #include<string.h>
 
 int contarVocales(char *expresion){
-	int i,vocal=0;
+	int i,vocal=0,A=0,E=0,I=0,O=0,U=0;
 	for(i=0;expresion[i]!='\0';i++){
 		char a=expresion[i];
-		if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u')
+		if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u'){
 			vocal++;
+			if(a=='a'){
+				A++;
+			}
+			if(a=='e'){
+				E++;
+			}
+			if(a=='i'){
+				I++;
+			}
+			if(a=='o'){
+				O++;
+			}
+			if(a=='u'){
+				U++;
+			}		
+		}
 	}
+	printf(" a's=%d\n e's=%d\n i's=%d\n o's=%d\n u's=%d\n",A,E,I,O,U);
 	return vocal;
-}
-
-int vocal_A(char *expresion){
-	int i,A=0;
-	for(i=0;expresion[i]!='\0';i++){
-		char a=expresion[i];
-		if(a=='a')
-			A++;
-	}
-	return A;
-}
-
-int vocal_E(char *expresion){
-	int i,E=0;
-	for(i=0;expresion[i]!='\0';i++){
-		char a=expresion[i];
-		if(a=='e')
-			E++;
-	}
-	return E;
-}
-
-int vocal_I(char *expresion){
-	int i,I=0;
-	for(i=0;expresion[i]!='\0';i++){
-		char a=expresion[i];
-		if(a=='i')
-			I++;
-	}
-	return I;
-}
-
-int vocal_O(char *expresion){
-	int i,O=0;
-	for(i=0;expresion[i]!='\0';i++){
-		char a=expresion[i];
-		if(a=='o')
-			O++;
-	}
-	return O;
-}
-
-int vocal_U(char *expresion){
-	int i,U=0;
-	for(i=0;expresion[i]!='\0';i++){
-		char a=expresion[i];
-		if(a=='u')
-			U++;
-	}
-	return U;
 }
 
 int main(){
@@ -71,16 +38,7 @@ int main(){
 	gets(expresion);
 	numVoc=contarVocales(expresion);
 	printf("El numero de vocales es %d\n",numVoc);
-	A=vocal_A(expresion);
-	printf("El numero de a's es %d\n",A);
-	E=vocal_E(expresion);
-	printf("El numero de e's es %d\n",E);
-	I=vocal_I(expresion);
-	printf("El numero de i's es %d\n",I);
-	O=vocal_O(expresion);
-	printf("El numero de o's es %d\n",O);
-	U=vocal_U(expresion);
-	printf("El numero de u's es %d\n",U);
+	
 	printf("Fin del programa\n");
 	return 0;
 }
