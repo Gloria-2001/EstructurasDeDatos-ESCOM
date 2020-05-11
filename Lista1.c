230da4882aad91a774e3dac88d2594ffb5e1a7f5
Gloria-2001 <olimen141@gmail.com>
2020-05-11 12:25:08 -0500

    Lista parte 1

--- Lista1.c
+++ Lista1.c
@@ -1,10 +1,9 @@
-#include <studio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct nodo{
 	int dato;
 	struct nodo *ptrSig; 
-	int llave;
 }Nodo;
 
 typedef struct Lista{
@@ -12,34 +11,76 @@ typedef struct Lista{
 	Nodo *ult;
 }Lista; 
 
-Nodo crearNodo(int dato1){
+Nodo *crearNodo(int dato1){
 	Nodo *ptrNuevo;
 	ptrNuevo=(Nodo*)malloc(sizeof(Nodo));
-	ptrNuevo->ptrSig=NULL;
 	ptrNuevo->dato=dato1;
-	return dato1;
+	ptrNuevo->ptrSig=NULL;
+	return ptrNuevo;
 }
 
-void meterDatos(Lista *list,int dato1){***************
-	Nodo *aux=q->ult;
-	
-	
-	
-	while(aux->ant!=NULL){
-		if(aux->ant->prio>aux->prio){
-			aux->ant->sig=aux->sig;
-			aux->ant=aux->ant->ant;
-			aux->sig=aux->ant->sig;
-			aux->sig->ant=aux;
-			aux->ant->sig=aux;
-			if(aux==q->ult){
-				q->ult=aux->sig;
-			}else{
-				aux->sig->sig->ant=aux->sig;
+void meterDatos(Lista *list,int dato1){
+	Nodo *ptrNuevo=crearNodo(dato1);
+	Nodo *auxNodo=list->pri;
+	if(list->pri>ptrNuevo){
+		ptrNuevo->ptrSig=list->pri;
+		list->pri=ptrNuevo;
+	} 
+	else if(list->ult<ptrNuevo){
+		list->ult->ptrSig=ptrNuevo;
+		list->ult=ptrNuevo;
+	}
+	else{
+		while(auxNodo->ptrSig!=NULL){
+			if(auxNodo->ptrSig > ptrNuevo){
+				ptrNuevo->ptrSig=auxNodo->ptrSig;
+				auxNodo->ptrSig=ptrNuevo;
 			}
-		}else
-			break;
+			auxNodo=auxNodo->ptrSig;
+		}
 	}
 }
 
-void recorrer(){}
+void recorrer(Lista *list){
+	Nodo *auxNodo=list->pri;
+	do{
+		printf("Dato: %d\n",auxNodo->dato);
+		auxNodo=auxNodo->ptrSig;
+	}while(auxNodo!=NULL);
+}
+
+int menu(){
+	int opc;
+	printf("1. Meter\n");
+	printf("2. Recorrer\n");
+	printf("3. Salir\n");
+	printf("La opcion elegida fue:");
+	scanf("%d",&opc);
+	return opc;
+}
+
+int main(){
+	Lista *list00=(Lista*)malloc(sizeof(Lista));
+	int miDato;
+	
+	while(1){
+		printf("Lista. Elija una opcion.\n");
+		switch(menu()){
+			case 1:
+				printf("Ingrese dato:\n");
+				scanf("%d",&miDato);
+				meterDatos(list00,miDato);
+				break;
+			case 2:
+				recorrer(list00);
+				break;
+			case 3:
+				exit(0);
+				break;
+			default:
+				printf("\nOpcion no existente, intente de nuevo.\n");
+				break;
+		}
+	}
+	return 0;
+}
